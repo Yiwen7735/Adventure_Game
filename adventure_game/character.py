7ae05b4ef21a89fd1bfbe2ec29409cd92829901a
Daniel Spencer <dms305@hku.hk>
2020-12-24 12:37:52 +0000

    Add base Character class for Player and Enemy
    This will be derived by Player and Enemy, along with later implemented
    NPCs.

---
+++ adventure_game/character.py
@@ -0,0 +1,24 @@
+class Character:
+    def __init__(self, name: str, hp: int):
+        self.name = name
+        self.hp = hp
+
+    def take_damage(self, points: int):
+        """
+        Inflict damage on the Character. Returns
+
+        Args:
+            points: The amount of hp damage to deal to the Character.
+
+        """
+        self.hp = max(0, self.hp - points)
+
+    def is_alive(self) -> bool:
+        """
+        Checks whether the Character is alive.
+
+        Returns:
+            True if the Character is alive, False otherwise.
+
+        """
+        return self.hp > 0
---
+++ tests/test_character.py
@@ -0,0 +1,20 @@
+import unittest
+
+from adventure_game.character import Character
+
+
+class CharacterTests(unittest.TestCase):
+    def test_initial_is_alive(self):
+        character = Character("Boy", 20)
+        self.assertEqual(character.is_alive(), True)
+
+    def test_initial_not_alive(self):
+        character = Character("Boy", 0)
+        self.assertEqual(character.hp, 0)
+        self.assertEqual(character.is_alive(), False)
+
+    def test_damaged_not_alive(self):
+        character = Character("Boy", 10)
+        character.take_damage(20)
+        self.assertEqual(character.hp, 0)
+        self.assertEqual(character.is_alive(), False)
