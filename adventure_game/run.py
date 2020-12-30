b3d10c2593df6a9d838b96fb8259b02245206bcf
Daniel Spencer <dms305@hku.hk>
2020-12-30 12:02:18 +0000

    Update game runner to enable room navigation

--- adventure_game/run.py
+++ adventure_game/run.py
@@ -1,18 +1,42 @@
+from typing import Iterable
+
+from .compass import Direction
 from .player import Player
-from . import room
+from .room import generate_first_room
+
+
+def print_options(options: Iterable[str]):
+    """
+    Prints the options, numbered 1 to len(options).
+
+    """
+    for i, option in enumerate(options):
+        print(f"{i + 1}. {option}")
 
 
 def run_game():
     name = input("Please enter your name: ")
     player = Player(name, 100)
 
-    curr_room = room.generate_room()
-    print(curr_room)
+    # Move the player to the starting room
+    player.move_to(generate_first_room())
 
-    options = curr_room.get_options()
-    for i, option in enumerate(options.keys()):
-        print(f"{i + 1}. {option}")
+    while True:
+        print("You enter " + str(player.current_room) + ".")
+        options = player.current_room.get_options()
+        if options:
+            print_options(options.keys())
+            action_id = input("Which action would you like to do?")
+            key = list(options.keys())[int(action_id) - 1]
+            # Invoke the action handler for the selected option
+            # TODO: some of the actions require further thought, e.g.:
+            #    - fight loop for attacking enemies
+            #    - printing treasure chest contents and allowing the player to
+            #      pick up the items (removing them from the chest)
+            options[key](player)
 
-    action_id = input("Which action would you like to do?")
-    key = list(options.keys())[int(action_id) - 1]
-    options[key](player)
+        print("Where would you like to go?")
+        exits = player.current_room.get_exits()
+        print_options(exits)
+        dest = Direction[exits[int(input("Enter you choice: ")) - 1]]
+        player.go(dest)
