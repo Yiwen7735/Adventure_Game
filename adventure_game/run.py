fc2c7cdf854fc2f74cff0ee03ed0084365b3d576
Daniel Spencer <dms305@hku.hk>
2020-12-30 19:41:30 +0000

    Update run to use new get_user_input function

--- adventure_game/run.py
+++ adventure_game/run.py
@@ -1,7 +1,7 @@
+from . import utils
 from .compass import Direction
 from .player import Player
 from .room import generate_first_room
-from .utils import print_options
 
 
 def game_over():
@@ -19,14 +19,22 @@ def run_game():
         print("You enter " + str(player.current_room) + ".")
         options = player.current_room.get_options()
         if options:
-            print_options(options.keys())
-            action_id = input("Which action would you like to do?")
+            action_id = None
+            while action_id is None:
+                utils.print_options(options.keys())
+                action_id = utils.get_user_input(
+                    "Which action would you like to do?",
+                    player
+                )
             key = list(options.keys())[int(action_id) - 1]
             # Invoke the action handler for the selected option
             options[key](player)
 
-        print("Where would you like to go?")
         exits = player.current_room.get_exits()
-        print_options(exits)
-        dest = Direction[exits[int(input("Enter you choice: ")) - 1]]
+        exit_idx = None
+        while exit_idx is None:
+            print("Where would you like to go?")
+            utils.print_options(exits)
+            exit_idx = utils.get_user_input("Enter your choice:", player)
+        dest = Direction[exits[exit_idx - 1]]
         player.go(dest)
