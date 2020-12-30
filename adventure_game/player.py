c2d2f43ceb1a2e8603d7604b8061fde76d75571b
Daniel Spencer <dms305@hku.hk>
2020-12-30 12:00:38 +0000

    Add Player documentation and movement method 'go'

--- adventure_game/player.py
+++ adventure_game/player.py
@@ -1,6 +1,7 @@
 from typing import List, Optional
 
 from .character import Character
+from .compass import Direction
 from .item import Item
 from .outfit import Outfit
 from .room import Room
@@ -23,11 +24,51 @@ class Player(Character):
         self.current_room: Optional[Room] = None
 
     def move_to(self, room: Room):
+        """
+        Moves the Player to the target Room.
+
+        Since this method is not relative to the Player's current location, it
+        is only intended to be called from outside the class for initial
+        placement of the Player. Internally, this condition is checked in
+        Player.go, so this method may be used safely therein.
+
+        Args:
+            room: The Room to which the Player should be moved.
+
+        """
         self.previous_room = self.current_room
         self.current_room = room
 
     def retreat(self):
+        """
+        Moves the Player to the previous Room.
+
+        Note: If the previous Room is uninitialized, the behavior of this method
+        is undefined.
+
+        """
         self.move_to(self.previous_room)
+
+    def go(self, direction: Direction):
+        """
+        Moves the Player in the target direction.
+
+        This implementation assumes that external checks guarantee that the
+        destination direction contains a Room. If this assumption does not hold,
+        a NoSuchExitException will be raised.
+
+        Args:
+            direction: The Direction in which the Player should move.
+
+        """
+        if direction == Direction.North:
+            self.move_to(self.current_room.north)
+        elif direction == Direction.South:
+            self.move_to(self.current_room.south)
+        elif direction == Direction.East:
+            self.move_to(self.current_room.east)
+        else:
+            self.move_to(self.current_room.west)
 
     def pick_up_item(self, item: Item):
         """
